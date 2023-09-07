f2c522cf1a5870c41b1925fdd393652749f62332
Saofee <saofee_wutthisamai@yahoo.com>
2023-09-07 23:54:03 +0700

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Saofee Wutthisamai'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Next.js for Test SSR on AWS Amplify'
 
 export default function Layout({ children, home }) {
   return (
